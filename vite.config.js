ab3367ecf3003c954cae3cedee0fb5342f18c1e4
Ayon Roy <ayonr1500@gmail.com>
2026-06-24 12:38:50 +0600

    fix vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/portfolio/",
+
   plugins: [react()],
+
   server: {
     host: "127.0.0.1",
     port: 5173,
   },
+
   build: {
     chunkSizeWarningLimit: 1100,
     rollupOptions: {
